e2f52d9b9d03712a8fbf14359e048fd77ee8399f
kayodegigz <kayogunrinde@gmail.com>
2022-03-11 16:17:07 -0800

    prints out numbers using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Prints out numbers
+ *Return: Returns 0 when successful
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+	putchar(ch);
+	}
+	return (0);
+	putchar('\n');
+}

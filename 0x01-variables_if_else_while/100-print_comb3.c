2af6baee9e743c465ec8736b59f09bae586c3aaf
kayodegigz <kayogunrinde@gmail.com>
2022-03-11 17:18:30 -0800

    if else

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+i#include <stdio.h>
+
+/**
+ * main - Prints combo of digits
+ *
+ * Return: 0 when code runs.
+*/
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		
+	}
+	putchar('\n');
+	return (0);
+}

f6e72d61880f8a0ad22b35b7f1fe508d476c92b7
kayodegigz <kayogunrinde@gmail.com>
2022-03-11 16:55:40 -0800

    print hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*main - Prints out all hexadecimal numbers
+*
+* Return: Returns 0 when successfully ran
+*/
+
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch)
+	}
+	putchar('\n');
+	return (0);
+}

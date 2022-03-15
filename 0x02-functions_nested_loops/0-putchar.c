db532e4c26b692620982ed5a50a0fc8715e33a1b
kayodegigz <kayogunrinde@gmail.com>
2022-03-15 16:50:14 -0700

    Prints out putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *main - The entry point of the function
+*
+* Return: The function returns a value of 0 when successful
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}

fd9ab84e81415b08bf9ba908ed4e92b3858036ee
kayodegigz <kayogunrinde@gmail.com>
2022-03-16 16:42:02 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,6 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
-		
 			if (j == 0)
 			{
 				_putchar(k + '0');
@@ -34,7 +33,8 @@ void times_table(void)
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
+
 		}
-	}
 	_putchar('\n');
+	}
 }

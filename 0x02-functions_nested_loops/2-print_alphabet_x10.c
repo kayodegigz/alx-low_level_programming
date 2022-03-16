0c4398b9992e1a6ed3097115904b3a7798483a97
kayodegigz <kayogunrinde@gmail.com>
2022-03-16 09:53:06 -0700

    prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+*print_alphabet - prints alphabets from a to z
+*/
+
+void print_alphabet(void)
+{
+	int a;
+
+	a = 0;
+
+	while (a < 10)
+	{
+		char ch;
+
+		ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar (ch);
+
+			_putchar ('\n');
+			ch++;
+		}
+		a++;
+	}
+}
+

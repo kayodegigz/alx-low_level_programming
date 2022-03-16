f78ffb9a1c443e747f2f372e6ce54997dc8ad68f
kayodegigz <kayogunrinde@gmail.com>
2022-03-15 17:14:31 -0700

    Prints out alphabets from a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+*main - Prints out alphabets from a to z
+*
+*Return:Does not return any value
+*/
+
+void print_alphabet(void)
+{
+
+	char ch = 'a'
+	while(ch <= z)
+	{
+		_putchar ch;
+		ch++
+	}
+	
+	_putchar('\n')
+}

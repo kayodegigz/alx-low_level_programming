2e2adedae7987291592e27821b0c89f0a3158243
kayodegigz <kayogunrinde@gmail.com>
2022-03-16 11:55:14 -0700

    prints the sign of the number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,9 +16,15 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	else
+	else if (n < 0)
 	{
 		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
 		return (0);
 	}
+	_putchar('\n');
 }

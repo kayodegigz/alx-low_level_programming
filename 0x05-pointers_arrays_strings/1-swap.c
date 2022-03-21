5b5916638a9013e57139f75993f4d0171526a071
kayogunrinde <kayogunrinde@gmail.com>
2022-03-22 00:02:26 +0100

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps two integers
+ * 
+ * @a: the first integer to be swapped 
+ * @b: the 2nd integer to be swapped
+ */
+void swap_int(int *a, int *b)
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}

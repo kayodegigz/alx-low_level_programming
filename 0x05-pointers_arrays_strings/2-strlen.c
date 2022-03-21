decdb08c87f9ac24a3270b2984c3ec39de457dc0
kayogunrinde <kayogunrinde@gmail.com>
2022-03-22 00:48:17 +0100

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - function gives the length ofd the string
+ *  
+ * @s: The character to be checked
+ */
+
+int _strlen(char *s)
+{
+        return (strlen(*s))
+}

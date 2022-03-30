589dc1eb449ae4bfd9c2cabc284eb6132357d5f5
kayodegigz <kayogunrinde@gmail.com>
2022-03-30 16:15:36 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
  * _puts_recursion - puth the character values on the screen
- * 
+ *
  * @s: The string to be outputted
- * 
+ *
  */
 
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        _putchar(*s);
-        _puts_recursion(s+1);
-    }
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }

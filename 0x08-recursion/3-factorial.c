9e8fc41d29e3304e3124bff0b9c6df4f661ed5fb
kayogunrinde <kayogunrinde@gmail.com>
2022-03-31 01:29:05 +0100

    recursions

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * factorial - func finds the factorial of a given number
+ * 
+ * @n: integer to be computed
+ * 
+ * Return: returns an int depending on the beaviour of the function 
+ */
+
+int factorial(int n)
+{
+    if (n < 0)
+    {
+        return(-1);
+    }
+    else if (n == 1)
+    {
+        return (0);
+    }
+    else
+    {
+        return (n * factorial(n-1));
+    }
+}

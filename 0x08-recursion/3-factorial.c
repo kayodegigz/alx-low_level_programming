8cff982ab46d0a0a9c57a60403371b5575041bf6
kayodegigz <kayogunrinde@gmail.com>
2022-03-30 17:36:13 -0700

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,24 +2,24 @@
 
 /**
  * factorial - func finds the factorial of a given number
- * 
+ *
  * @n: integer to be computed
- * 
- * Return: returns an int depending on the beaviour of the function 
+ *
+ * Return: returns an int depending on the beaviour of the function
  */
 
 int factorial(int n)
 {
-    if (n < 0)
-    {
-        return(-1);
-    }
-    else if (n == 0)
-    {
-        return (1);
-    }
-    else
-    {
-        return (n * factorial(n-1));
-    }
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }

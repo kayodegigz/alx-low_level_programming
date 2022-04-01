b53caa551e4c81045ad30148484847342952358d
kayodegigz <kayogunrinde@gmail.com>
2022-04-01 13:36:11 -0700

    args argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,13 @@
 
 /**
  * main - prints the number of arguments passed into it
- * 
+ *
  * @argc: no of arguments
  * @argv: array of arguments passed
- * @return 0 is successful
+ * Return: 0 is successful
  */
 int main(int argc, char *argv)
 {
-        printf("%d", argc -1);
-        return (0);
+	printf("%d", argc - 1);
+	return (0);
 }

3d93c3bdab2356602ea5f0e5f1749f6800f9219d
kayogunrinde <kayogunrinde@gmail.com>
2022-04-02 00:20:41 +0100

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - prints the multiplication of two ints
  *
@@ -15,6 +16,6 @@ int main(int argc, char *argv)
     }
     else
     {
-        printf("%d\n", atoi(argv[1] * argv[2]));
+        printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
     }
 }

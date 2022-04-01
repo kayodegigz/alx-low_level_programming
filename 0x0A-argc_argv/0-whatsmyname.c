03d6ed7182f17b9afa36a0a6869295420d665738
kayogunrinde <kayogunrinde@gmail.com>
2022-04-01 20:27:34 +0100

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * @argc: the number of arguments in the array name
+ * @argv: the one dimensional array of the arguments
+ * Return: returns an integer value
+ */
+int main(int argc, char *argv)
+{
+    printf("%s",argv[0]);
+    return (0);
+}

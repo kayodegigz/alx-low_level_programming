a156e771387cf7c82c5fb93357e266cdd13361f1
kayogunrinde <kayogunrinde@gmail.com>
2022-04-01 21:03:42 +0100

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * @argv: the one dimensional array of the arguments
  * Return: returns an integer value
  */
-int main(int argc, char *argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-    printf("%s\n",argv[0]);
+    printf("%s\n", argv[0]);
     return (0);
 }

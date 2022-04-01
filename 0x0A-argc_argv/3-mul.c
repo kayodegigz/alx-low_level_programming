fbcc42a46467760aa9a64a56a18e76f00f0df2d4
kayodegigz <kayogunrinde@gmail.com>
2022-04-01 16:26:15 -0700

    args argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,17 +5,17 @@
  *
  * @argc: the number of arguments
  * @argv: string array of arguments
- * return: 1 if unsuccessful, 0 if successful
+ * Return: 1 if unsuccessful, 0 if successful
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    else
-    {
-        printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
 }

41a4be1c766152271b9df0c512d46225964add15
kayogunrinde <kayogunrinde@gmail.com>
2022-04-01 21:37:27 +0100

    argc argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * @argv: array of arguments passed
  * Return: 0 is successful
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv __attribute__((unused)))
 {
 	printf("%d", argc - 1);
 	return (0);

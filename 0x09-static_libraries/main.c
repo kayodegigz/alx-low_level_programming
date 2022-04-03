b60ab115c917d4bb41ec4ec9ab123a9f08c6a633
kayodegigz <kayogunrinde@gmail.com>
2022-04-03 13:30:00 -0700

    static libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,11 @@
 #include "main.h"
-
+/**
+ * main - puts out a sentence
+ *
+ * Return: returns 0 if successful
+ */
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	    return (0);
+	return (0);
 }

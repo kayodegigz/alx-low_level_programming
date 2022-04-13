dec24030a16af9da2fcf225b931f44347dc505d5
kayogunrinde <kayogunrinde@gmail.com>
2022-04-13 16:38:16 +0100

    structs and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,14 @@
+/**
+ * struct dog - contains the details of the dog
+ * @name: name of the dog
+ *
+ * @age: the age of the dog
+ *
+ * @owner: owner of te dog
+ *
+ * Description: the structure contains details of the dog
+ */
+
 #ifndef _dog_
 #define _dog_
 

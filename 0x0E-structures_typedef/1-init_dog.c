4d78881520c12c96b56aed5b7704ef58f1acb63b
kayogunrinde <kayogunrinde@gmail.com>
2022-04-13 17:06:21 +0100

    structs and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * struct dog - contains details of the dog
+ * 
+ * @d: the name of the struct
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Description: contains the details of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    if (d)
+    {
+        (*d).name = name;
+        (*d).age = age;
+        (*d).owner = owner;
+    }
+}
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,6 @@
+#ifndef _main_
+#define _main_
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif

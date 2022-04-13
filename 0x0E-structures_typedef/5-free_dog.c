07da98535b53ac469ecb8f96ca87add3923a2851
kayogunrinde <kayogunrinde@gmail.com>
2022-04-13 18:57:11 +0100

    structs and typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees the memory space for dog
+ *
+ * @d: the pointer to a memory space
+ */
+void free_dog(dog_t *d)
+{
+    if (d)
+    {
+        free((*d).name);
+        free((*d).owner);
+        free(d);
+    }
+}

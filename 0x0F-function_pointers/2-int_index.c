00627579fcceb48d694b6151e8280440fcd0edc2
kayogunrinde <kayogunrinde@gmail.com>
2022-04-14 23:16:41 +0100

    function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,14 +11,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    if (size <= 0)
-    {
-        return (-1);
-    }
-
     if (array && cmp)
     {
         unsigned int i;
+
+        if (size <= 0)
+        {
+            return (-1);
+        }
 
         for (i = 0; i < size; i++)
         {

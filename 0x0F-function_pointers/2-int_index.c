f22b95e6c87845ba2ad03c23aa054661af2f4221
kayogunrinde <kayogunrinde@gmail.com>
2022-04-14 23:13:22 +0100

    function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that returns an index
+ *
+ * @array: the array of ints to be checked
+ * @size: the size of the array
+ * @cmp: a pointer to the array
+ * Return: returns -1 if the function is unsuccessful
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    if (size <= 0)
+    {
+        return (-1);
+    }
+
+    if (array && cmp)
+    {
+        unsigned int i;
+
+        for (i = 0; i < size; i++)
+        {
+            if (cmp(array[i]))
+            {
+                return (i);
+            }
+        }
+        return (-1);
+    }
+}

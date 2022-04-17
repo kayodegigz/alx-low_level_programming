6a1e88e1131e988c8d60261fd4e7fa0550936bb4
kayogunrinde <kayogunrinde@gmail.com>
2022-04-17 15:54:39 +0100

    variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,17 +16,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	if (separator)
-	{
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(ap, int));
-			if (i < (n - 1))
+			if (separator && i < (n - 1))
 			{
 				printf("%s", separator);
 			}
 		}
-	}
-    va_end(ap);
 	printf("\n");
+    va_end(ap);
 }

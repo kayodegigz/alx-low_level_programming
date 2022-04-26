864a93c0873a6cbf67bcf368750b1b7c94ac8ed5
kayodegigz <kayogunrinde@gmail.com>
2022-04-26 13:54:37 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-    size_t node_count;
+	size_t node_count;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        node_count++;
-    }
-    return (node_count);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
 }

b96124a4b63624368152f122d7bfb2906ff4f12e
kayogunrinde <kayogunrinde@gmail.com>
2022-04-26 22:44:45 +0100

    singly linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * listint_len - a function that returns the number of elements
+ *
+ * @h: pointer to the first node
+ *
+ * Return: returns a size_t data type
+ */
+
+size_t listint_len(const listint_t *h)
+{
+    size_t num_elements;
+    num_elements = 0;
+
+    while (h)
+    {
+        h = h->next;
+        num_elements++;    
+    }
+    return (num_elements);
+}

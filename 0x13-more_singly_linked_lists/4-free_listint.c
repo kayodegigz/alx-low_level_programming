cc79294c09d23adf08a8b84e3da3ca393767b05f
kayogunrinde <kayogunrinde@gmail.com>
2022-04-27 01:52:34 +0100

    singly linked lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - program frees a singly linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *new;
+
+	while (new = head)
+	{
+		head = head->next;
+		free(new);
+	}
+}

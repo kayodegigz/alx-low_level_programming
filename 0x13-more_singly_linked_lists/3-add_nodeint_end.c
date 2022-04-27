6bf65992c4cc9e4a6c03acf16d873cdc057b5f37
kayodegigz <kayogunrinde@gmail.com>
2022-04-26 17:07:57 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,28 +1,40 @@
 #include "lists.h"
+/**
+ * add_nodeint_end - adds a node to the end of the list
+ *
+ * @head: address of the first node
+ *
+ * @n: the value to be added to the node
+ *
+ * Return: returns an address to the final node
+ */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new_node;
-    new_node = malloc(sizeof(listint_t));
+	listint_t *new_node;
 
-    listint_t *last = *head;
-    if (new_node)
-        new_node->n = n;
-        new_node->next = NULL;
+	new_node = malloc(sizeof(listint_t));
 
-        if (*head == NULL) /*If the node is empty*/
-        {
-            *head = new_node;
-            return (new_node);
-        }
+	listint_t *last = *head;
 
-        while (last->next != NULL)
-        {
-            last = last->next;
-        }
-        
-        last->next = new_node;
-        return(new_node);
+	if (new_node)
+	{	new_node->n = n;
+		new_node->next = NULL;
 
-    return (NULL);
+		if (*head == NULL) /*If the node is empty*/
+		{
+			*head = new_node;
+			return (new_node);
+		}
+
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+
+		last->next = new_node;
+		return (new_node);
+	}
+
+	return (NULL);
 }

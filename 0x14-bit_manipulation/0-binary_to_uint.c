65af5fe4e435df4c2a17e75ee8686801e67bba3a
kayogunrinde <kayogunrinde@gmail.com>
2022-04-29 00:57:31 +0100

    bitwise operators

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary string to a decimal number
+ *
+ * @b: the string of binary ints
+ *
+ * Return: returns an unsigned int value
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+    int i = 0;
+    int len = 0; /*going to store the len of the string*/
+    int raised_to_power = 1;
+    unsigned int dec_value = 0;
+
+    while (b[i] != '\0')
+    {
+        len++;
+    }
+    
+    for (i = 0; i <= len; i++)
+    {
+        if (b[i] == '1')
+        {
+            dec_value += raised_to_power;
+        }
+        raised_to_power *= 2;
+    }
+    return (dec_value);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef _bitwise_
+#define _bitwise_
+
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+
+
+#endif

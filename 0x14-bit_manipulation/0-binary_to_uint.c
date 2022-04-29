1acf120a0cd3945a466e275c81404150a2255789
kayodegigz <kayogunrinde@gmail.com>
2022-04-28 17:03:03 -0700

    bitwise operators

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,23 +10,23 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    int i = 0;
-    int len = 0; /*going to store the len of the string*/
-    int raised_to_power = 1;
-    unsigned int dec_value = 0;
+	int i = 0;
+	int len = 0; /*going to store the len of the string*/
+	int raised_to_power = 1;
+	unsigned int dec_value = 0;
 
-    while (b[i] != '\0')
-    {
-        len++;
-    }
-    
-    for (i = 0; i <= len; i++)
-    {
-        if (b[i] == '1')
-        {
-            dec_value += raised_to_power;
-        }
-        raised_to_power *= 2;
-    }
-    return (dec_value);
+	while (b[i] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i <= len; i++)
+	{
+		if (b[i] == '1')
+		{
+			dec_value += raised_to_power;
+		}
+		raised_to_power *= 2;
+	}
+	return (dec_value);
 }

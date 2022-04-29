1f5b6ac2cd18b6b89b5b218e805923c204a12b10
kayodegigz <kayogunrinde@gmail.com>
2022-04-29 15:07:37 -0700

    bitwise operators

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,21 +3,21 @@
 /**
  * set_bit - sets the bit value at a particular index to 1
  *
- * @*n: an unsigned long int
+ * @n: an unsigned long int
  * @index: the index whose bit value will be changed
  * Return: returns an int value
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned int bit_or_checker;
-    
-    if (index > 64)
-    {
-        return (-1);
-    }
+	unsigned int bit_or_checker;
 
-    bit_or_checker = 1 << index;
-    *n = (*n | bit_or_checker);
-    return (*n);
+	if (index > 64)
+	{
+		return (-1);
+	}
+
+	bit_or_checker = 1 << index;
+	*n = (*n | bit_or_checker);
+	return (*n);
 }

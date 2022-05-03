9f6a9ac76059facb564ca29435ca18eac01cd943
kayodegigz <kayogunrinde@gmail.com>
2022-05-03 08:12:24 -0700

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,33 +10,33 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytes_read, bytes_written; /*bytes successfully written and read*/
-    char *buf; /*buffer for temporarily storing the text*/
+	int fd;
+	ssize_t bytes_read, bytes_written; /*bytes successfully written and read*/
+	char *buf; /*buffer for temporarily storing the text*/
 
-    if (filename == NULL)
-    {
-        return (0);
-    }
+	if (filename == NULL)
+	{
+		return (0);
+	}
 
-    fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-    if (fd < 0)
-    {
-        return (0);
-    }
+	if (fd < 0)
+	{
+		return (0);
+	}
 
-    buf = malloc(sizeof(char) * letters);
+	buf = malloc(sizeof(char) * letters);
 
-    bytes_read = read(fd, buf, letters);
-    if (bytes_read < 0)
-    {
-        free (buf);
-        return (0);
-    }
-    bytes_written = write(1, buf, bytes_read);
+	bytes_read = read(fd, buf, letters);
+	if (bytes_read < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	bytes_written = write(1, buf, bytes_read);
 
-    return (bytes_read); /*or bytes written, anyone*/
-    free(buf);
-    close(fd);
+	return (bytes_read); /*or bytes written, anyone*/
+	free(buf);
+	close(fd);
 }

8fe7cffd7c5d7bdf0cefd1b409a5da0c1191a11f
kayogunrinde <kayogunrinde@gmail.com>
2022-05-03 16:01:35 +0100

    file input/output

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef _FILEIO_
+#define _FILEIO_
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+
+
+
+
+#endif

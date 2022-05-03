bde34169ed0568008f1d378393b1a7f6367a39df
kayodegigz <kayogunrinde@gmail.com>
2022-05-03 12:21:26 -0700

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fp = open("filename", O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fp < 0)
 	{
 		return (-1);

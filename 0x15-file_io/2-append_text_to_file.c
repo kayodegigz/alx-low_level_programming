798e2aa9f97cb2de865614dea22a0843d76d4050
kayodegigz <kayogunrinde@gmail.com>
2022-05-03 14:25:41 -0700

    file io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,21 +22,19 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content == NULL)
+	if (text_content)
 	{
-		text_content = "";
-	}
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
 
-	wrt = write(fd, text_content, i);
-	if (wrt < 0)
-	{
-		return (-1);
+		wrt = write(fd, text_content, i);
+		if (wrt < 0)
+		{
+			return (-1);
+		}
 	}
-
 	close(fd);
 	return (1);
 }

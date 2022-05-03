512ab6264d96c2a79f2a7e72440f648254645d10
kayodegigz <kayogunrinde@gmail.com>
2022-05-03 12:16:02 -0700

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,35 +10,35 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fp, wrt;
-    int i = 1, j = 0;
+	int fp, wrt;
+	int i = 1, j = 0;
 
-    if (!filename)
-    {
-        return (-1);
-    }
+	if (!filename)
+	{
+		return (-1);
+	}
 
-    fp = open("filename", O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fp < 0)
-    {
-        return (-1);
-    }
+	fp = open("filename", O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fp < 0)
+	{
+		return (-1);
+	}
 
-    while (text_content[j] != '\0')
-    {
-        i++;
-    }
-    
-    if (text_content == NULL)
-    {
-        write(fp, "", i);
-    }
+	while (text_content[j] != '\0')
+	{
+		i++;
+	}
 
-    wrt = write(fp, text_content, i);
-    if (wrt < 0)
-    {
-        return (-1);
-    }
-    close(fp);
-    return (1);
+	if (text_content == NULL)
+	{
+		write(fp, "", i);
+	}
+
+	wrt = write(fp, text_content, i);
+	if (wrt < 0)
+	{
+		return (-1);
+	}
+	close(fp);
+	return (1);
 }

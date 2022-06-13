0ebf05b5ea6637a931725da36f921e3f84d91d15
kayodegigz <kayogunrinde@gmail.com>
2022-06-13 17:33:00 +0100

    trying to fix bugs

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,6 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = moving_node->next;
 		free(moving_node);
+		return (1);
 	}
 
 	while (moving_node->next != NULL && node_counter < index)

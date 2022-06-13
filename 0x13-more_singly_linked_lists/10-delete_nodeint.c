b747ea6a78a6ccd17fc228d6961e5daf4e43c692
kayodegigz <kayogunrinde@gmail.com>
2022-06-13 17:45:22 +0100

    trying to fix out of range bugs

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		node_counter++;
 	}
 
-	if (moving_node == NULL && index > node_counter)
+	if (moving_node->next == NULL && index > node_counter)
 	{
 		return (-1); 
 	/**means the index is out of range cos if it wasn't it'd not reach the NULL address*/

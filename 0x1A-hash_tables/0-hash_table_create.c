4d7650f928ff98048b4c732d8b7c1a8310fff5a6
kayodegigz <kayogunrinde@gmail.com>
2022-07-02 19:47:30 +0100

    did not have to initialize values to NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_t = malloc(sizeof(hash_table_t));
-	/**unsigned int i;*/
 
 	if (new_hash_t == NULL)
 	{
@@ -19,17 +18,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	new_hash_t->size = size;
-	new_hash_t->array = malloc(sizeof(hash_node_t*) * size);
+	new_hash_t->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (new_hash_t->array == NULL)
 	{
 		return (NULL);
 	}
-	/**
-	for(i = 0; i < size; i++)
-	{
-		new_hash_t->array[i] = NULL;
-	}
-	*/
+
 	return (new_hash_t);
 }

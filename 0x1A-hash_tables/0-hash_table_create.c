e383cb433c2c98c81fdd0a9beb94e6d2652cbec3
kayodegigz <kayogunrinde@gmail.com>
2022-07-02 19:42:07 +0100

    initializing array values to NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	new_hash_t->size = size;
-	new_hash_t->array = malloc(sizeof(hash_node_t) * size);
+	new_hash_t->array = malloc(sizeof(hash_node_t*) * size);
 
 	if (new_hash_t->array == NULL)
 	{

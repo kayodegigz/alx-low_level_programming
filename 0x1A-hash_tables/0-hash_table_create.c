b314e3c453efe28759ac0e703362289fc23ed457
kayodegigz <kayogunrinde@gmail.com>
2022-07-02 19:18:23 +0100

    creating a new hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a new hash table
+ *
+ * @size: The size of the hash table
+ *
+ * Return: returns the pointer to the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_t = malloc(sizeof(hash_table_t) * size);
+
+	if (new_hash_t == NULL)
+	{
+		return (NULL);
+	}
+	return (new_hash_t);
+}

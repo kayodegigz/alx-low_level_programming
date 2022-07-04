61711a547f9ee3b8fd212d24981dc54ec970ad3a
kayodegigz <kayogunrinde@gmail.com>
2022-07-04 21:04:34 +0100

    freeing a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *tmp;
+
+	if (ht)
+	{
+		while (i < ht->size)
+		{
+			tmp = ht->array[i];
+			while (tmp != NULL)
+			{
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+				tmp = tmp->next;
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+}

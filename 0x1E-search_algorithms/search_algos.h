22e56bd4a7cea2149342ac9615365d1323befbdc
kayodegigz <kayogunrinde@gmail.com>
2022-10-04 23:55:33 +0100

    binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,7 @@
 #include <stdio.h>
 #include <stddef.h>
 int linear_search(int *array, size_t size, int value);
+int recursive_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
